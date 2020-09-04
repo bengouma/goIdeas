5d746d1b027dc84cbf55da14dbe90b9db1c3d7b3
bgoumalatsos <bgoumalatsos@bethel.jw.org>
2020-09-04 13:56:48 -0400

    Add file for scraper project

---
+++ scraper/scrape.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func processSite(index int, element *goquery.Selection) {
+	div, exists := element.Attr("class")
+	if exists {
+		fmt.Println(div)
+	}
+}
+
+func main() {
+	response, err := http.Get("https://bgoumalatsos.com")
+	if err != nil {
+		fmt.Println("Error fetching site")
+	}
+	defer response.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		fmt.Println("Error copying site body to output")
+	}
+	doc.Find("div").Each(processSite)
+}
